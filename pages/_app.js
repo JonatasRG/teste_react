6dae5d1f8bf7847d30858053d640ca5e899702d3
JonatasRG <70361185+JonatasRG@users.noreply.github.com>
2021-02-02 21:01:54 -0300

    primeiro commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,69 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db  from '../db.json'
 
-const GlobalStyle = createGlobalStyle`
+/*const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-`
+`*/
 
-const theme = {
+
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+const theme = db.theme
+/*
+{
   colors: {
-    primary: '#0070f3',
+    primary: '#00bcd4',
+             
+    secondary : '#00e676',
   },
 }
+*/
+
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+      <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
